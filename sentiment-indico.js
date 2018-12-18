99fbbb67bcab0f938c89aa052a46acbc2c90e9b5
Jalia Evans <irohdesign@outlook.com>
2018-12-17 20:46:29 -0500

    added ability to distinguish between the different emotion results in the reponse with if statements and checking if the object returned has a certain property name, because indico allows for a threshold to be set

--- sentiment-indico.js
+++ sentiment-indico.js
@@ -1,16 +1,8 @@
-// // single example
-// $.ajax(
-//     'https://apiv2.indico.io/sentiment',
-//     JSON.stringify({
-//       'api_key': "472f187a7c554f42d719990a387ff96c",
-//       'data': "I love writing code!",
-//       method: "GET"
-//   ).then(function(response){
-//        console.log(res) 
-//         }
-//   );
 
-var userInput = "I'm feeling great today!";
+
+$("#feelingsButton").on
+
+var userInput = $("#emotion-input").val();
   
   // batch example
   $.post(
@@ -22,7 +14,22 @@ var userInput = "I'm feeling great today!";
     })
   ).then(function(response) { 
 
+  if (response.results.hasOwnProperty('anger')) {
+    console.log("Anger");
+  } else if (response.results.hasOwnProperty('joy')) {
+    console.log("Joy");
+  } else if (response.results.hasOwnProperty('fear')) {
+    console.log("Fear");
+  } else if (response.results.hasOwnProperty('sadness')) {
+    console.log("Sadness");
+  } else if (response.results.hasOwnProperty('surprise')) {
+    console.log("Surprise");
+  } else {
+    console.log("Didn't get an emotion off this. Sorry.");
+  }
+
     console.log(response.results);
+    // $("#result").text("The user is feeling ";
     console.log(response)
 
   });
