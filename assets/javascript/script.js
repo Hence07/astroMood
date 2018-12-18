1aefbe0dc790b6044b351de56227b7dcee240eaf
Jalia Evans <irohdesign@outlook.com>
2018-12-18 12:03:00 -0500

    working on zodiac function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,13 +2,49 @@
 var birthdate;
 var horoscope;
 var predictedDay;
+var noYear;
+var noSlash;
+var birthString;
+var splitDate;
+var userMonth;
+var userDate;
+
+
 
 // Attach a submit handler to the form
 $("#zodiacForm").submit(function (event) {
 
+    event.preventDefault();
+
+    //grab date from datepicker for zodiac sign function
+        //get rid of last 4 digits
+        birthdate = $("#datepicker").val();
+
+        noYear = birthdate.slice(0, -5);
+        splitDate = noYear.split("/");
+        userMonth = splitDate[0];
+        userDate = splitDate[1];
+        
+
+        console.log(birthdate);
+        console.log(noYear);
+        console.log(splitDate);
+        console.log(userMonth);
+        console.log(userDate);
+
+        //create if statements to calculate the range of dates for birthdays
+
+        //aries mar 21 - apr 19
+        if ((userMonth == 03 && userDate >= 21) || (userMonth == 04 && userDate <= 19))  {
+            console.log("they're an aries")
+        }
+
+        //assign zodiac signs for date ranges
+
+
 
     // Stop form from submitting normally
-    event.preventDefault();
+    
 
     // Get some values from elements on the page:
         birthdate = $("#datepicker").val();
