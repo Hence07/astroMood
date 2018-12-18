5e694f645252ac6ff2d2513884eb3958c7b42db1
Jalia Evans <irohdesign@outlook.com>
2018-12-17 19:38:47 -0500

    added svg files for each zodiac sign

--- sentiment-indico.js
+++ sentiment-indico.js
@@ -16,8 +16,7 @@
     JSON.stringify({
       'api_key': "472f187a7c554f42d719990a387ff96c",
       'data': [
-        "I love writing code!",
-        "Alexander and the Terrible, Horrible, No Good, Very Bad Day"
+        "There will be some big news today—and some wildly different first reactions to it. Don't assume that other people are feeling the same way you are—that kind of assumption will lead to problems. Wait until the dust has settled, and then do a casual inventory of other people's feelings. Once you get an idea of what everyone thinks, you will know what sort of response to suggest—and everyone will be much happier for it."
       ]
     })
   ).then(function(res) { console.log(res) });
