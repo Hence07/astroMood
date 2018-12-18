bfed3becd496edc16a211df3d2b9ababde72a15b
Jalia Evans <irohdesign@outlook.com>
2018-12-18 10:45:40 -0500

    decided to use sentiment api instead of emotions, because positive vs negative is simpler with the horoscope. also combined horoscope and sentiment api calls into one file for ease of use

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,11 @@
+//variables
+var anger;
+var joy;
+var fear;
+var sadness;
+var surprise;
+var horoscope;
+var predictedDay;
 
 // Attach a submit handler to the form
 $("#searchForm").submit(function (event) {
@@ -18,9 +26,61 @@ $("#searchForm").submit(function (event) {
     // Put the results in a div
     posting.done(function (data) {
         //var content = $(data);
-        var content = data.description;
-        $("#result").empty().append("RESPONSE: <pre>" + JSON.stringify(content) + "</pre>");
+        horoscope = data.description;
+        // $("#result").empty().append("RESPONSE: <pre>" + JSON.stringify(content) + "</pre>");
+        console.log(horoscope);
+
+        $.post(
+            'https://apiv2.indico.io/sentiment',
+            JSON.stringify({
+              'api_key': "472f187a7c554f42d719990a387ff96c",
+              'data': horoscope
+            })
+          ).then(function(response) { 
+        
+            console.log(response);
+        //   if (response.results.hasOwnProperty('anger')) {
+        //     anger == true;
+        //     console.log("Anger");
+        //   }
+          
+        //   if (response.results.hasOwnProperty('joy')) {
+        //     joy == true;
+        //     console.log("Joy");
+        //   }
+          
+        //   if (response.results.hasOwnProperty('fear')) {
+        //     fear == true;
+        //     console.log("Fear");
+        //   } 
+          
+        //   if (response.results.hasOwnProperty('sadness')) {
+        //     sadness == true;
+        //     console.log("Sadness");
+        //   } 
+          
+        //   if (response.results.hasOwnProperty('surprise')) {
+        //     surprise == true;
+        //     console.log("Surprise");
+        //   } 
+
+        
+            //clear userInput value
+            if (response.results >= 0.5) {
+                console.log("You're going to have a good day.")
+            } else {
+                console.log("You might have a bad day.")
+            }
+        
+          });
+        
+        });
         
     });
+
+// $("#feelingsButton").on("click", function() {
+  
+  // batch example
+ 
     
-});
+// });
