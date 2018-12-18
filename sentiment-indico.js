78a2af946b54fe13f5dfd1633972a1e39cabaa98
Jalia Evans <irohdesign@outlook.com>
2018-12-17 20:50:40 -0500

    raised the threshold to 0.5 to get rid of excess emotions that don't really fit the description. added boolean variables for easier manipulation of the data

--- sentiment-indico.js
+++ sentiment-indico.js
@@ -1,6 +1,10 @@
+var anger;
+var joy;
+var fear;
+var sadness;
+var surprise;
 
-
-$("#feelingsButton").on
+$("#feelingsButton").on("click", function() {
 
 var userInput = $("#emotion-input").val();
   
@@ -10,26 +14,40 @@ var userInput = $("#emotion-input").val();
     JSON.stringify({
       'api_key': "472f187a7c554f42d719990a387ff96c",
       'data': userInput,
-      'threshold' : 0.1
+      'threshold' : 0.5
     })
   ).then(function(response) { 
 
   if (response.results.hasOwnProperty('anger')) {
+    anger == true;
     console.log("Anger");
-  } else if (response.results.hasOwnProperty('joy')) {
-    console.log("Joy");
-  } else if (response.results.hasOwnProperty('fear')) {
-    console.log("Fear");
-  } else if (response.results.hasOwnProperty('sadness')) {
-    console.log("Sadness");
-  } else if (response.results.hasOwnProperty('surprise')) {
-    console.log("Surprise");
-  } else {
-    console.log("Didn't get an emotion off this. Sorry.");
   }
+  
+  if (response.results.hasOwnProperty('joy')) {
+    joy == true;
+    console.log("Joy");
+  }
+  
+  if (response.results.hasOwnProperty('fear')) {
+    fear == true;
+    console.log("Fear");
+  } 
+  
+  if (response.results.hasOwnProperty('sadness')) {
+    sadness == true;
+    console.log("Sadness");
+  } 
+  
+  if (response.results.hasOwnProperty('surprise')) {
+    surprise == true;
+    console.log("Surprise");
+  } 
+  
 
     console.log(response.results);
     // $("#result").text("The user is feeling ";
     console.log(response)
 
   });
+
+});
