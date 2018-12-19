04fce73c663d1af4ba338a56da8e7067a684a83e
yji819 <loemsed@gmail.com>
2018-12-19 14:08:41 -0500

    splited function into two, created extra vars to store localstoragedata, added function to toggle form to resultpage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,6 +9,17 @@ var userSign;
 var sentiment;
 var barWidth;
 var stringMonth;
+var userName;
+var form = $("#zodiacForm");
+var result = $("#results");
+var formToggle = localStorage.getItem("data");
+var sign
+var name
+var images
+
+
+resultPage();
+
 
 $(function() {
     $( "#datepicker" ).datepicker({
@@ -17,26 +28,39 @@ $(function() {
     });
 })
 
+
+if (formToggle == "true") {
+    form.hide();
+    result.show();
+   }
+ if (formToggle != "true") {
+   result.hide();
+   form.show();
+   
+ }
+
+
 // Attach a submit handler to the form
 $(document).on("click", "#send", function (event) {
 
     event.preventDefault();
 
-
+   
+    
     //use animate.css to shake the button if there's no input for the calendar or name
     if (($('#full-name').val() == '') || ($("#date-picker").val() == '')) {
         $(this).addClass("animated shake");
     }
-
-
+    
+    userName = $("#full-name").val();
     //grab date from datepicker for zodiac sign function
-        //get rid of last 4 digits
-        birthdate = $("#datepicker").val();
+    //get rid of last 4 digits
+    birthdate = $("#datepicker").val();
 
-        splitDate = birthdate.split(" ");
-        userDate = splitDate[0];
+    splitDate = birthdate.split(" ");
+    userDate = splitDate[0];
         stringMonth = splitDate[1];
-
+        
         if (stringMonth == "January") {
             userMonth = 1;
         }
@@ -76,16 +100,16 @@ $(document).on("click", "#send", function (event) {
             userMonth = 12;
         }
         
-
+        
         //create if statements to calculate the range of dates for birthdays
-
-
+        
+        
         //aries mar 21 - apr 19
         if ((userMonth == 03 && userDate >= 21) || (userMonth == 04 && userDate <= 19))  {
             userSign = "aries";
             zodiacImg = "./assets/img/zodiac-svg/aries-symbol.svg";
         }
-
+        
         //taurus apr 20 - may 20
         if ((userMonth == 04 && userDate >= 20) || (userMonth == 05 && userDate <= 20))  {
             userSign = "taurus";
@@ -97,81 +121,96 @@ $(document).on("click", "#send", function (event) {
             userSign = "gemini";
             zodiacImg = "./assets/img/zodiac-svg/gemini-zodiac-symbol-of-two-twins-faces.svg";
         }
-
+        
         //cancer jun 21 - jul 22
         if ((userMonth == 06 && userDate >= 21) || (userMonth == 07 && userDate <= 22))  {
             userSign = "cancer";
             zodiacImg = "./assets/img/zodiac-svg/cancer-astrological-sign-of-crab-zodiac-sign.svg";
         }
-
+        
         //leo jul 23 - aug 22
         if ((userMonth == 07 && userDate >= 23) || (userMonth == 08 && userDate <= 22))  {
             userSign = "leo";
             zodiacImg = "./assets/img/zodiac-svg/leo-astrological-sign.svg";
         }
-
+        
         //virgo aug 23 - sep 22
         if ((userMonth == 08 && userDate >= 23) || (userMonth == 09 && userDate <= 22))  {
             userSign = "virgo";
             zodiacImg = "./assets/img/zodiac-svg/virgo-woman-head-shape-symbol.svg";
         }
-
+        
         //libra sep 23 - oct 22
         if ((userMonth == 09 && userDate >= 23) || (userMonth == 10 && userDate <= 22))  {
             userSign = "libra";
             zodiacImg = "./assets/img/zodiac-svg/libra-scale-balance-symbol.svg";
         }
-
+        
         //scorpio oct 23 - nov 21
         if ((userMonth == 10 && userDate >= 23) || (userMonth == 11 && userDate <= 21))  {
             userSign = "scorpio";
             zodiacImg = "./assets/img/zodiac-svg/scorpion-shape-of-zodiac-sign.svg";
         }
-
+        
         //sagittarius nov 22 - dec 21
         if ((userMonth == 11 && userDate >= 22) || (userMonth == 12 && userDate <= 21))  {
             userSign = "sagittarius";
             zodiacImg = "./assets/img/zodiac-svg/sagittarius-arch-and-arrow-symbol.svg";
         }
-
+        
         //capricorn dec 22 - jan 19
         if ((userMonth == 12 && userDate >= 22) || (userMonth == 01 && userDate <= 19))  {
             userSign = "capricorn";
             zodiacImg = "./assets/img/zodiac-svg/capricorn-goat-animal-shape-of-zodiac-sign.svg";
         }
-
+        
         //aquarius jan 20 - feb 18
         if ((userMonth == 01 && userDate >= 20) || (userMonth == 02 && userDate <= 18))  {
             userSign = "aquarius";
             zodiacImg = "./assets/img/zodiac-svg/aquarius-water-container-symbol.svg";
         }
-
+        
         //pisces feb 19 - mar 20
         if ((userMonth == 02 && userDate >= 19) || (userMonth == 03 && userDate <= 20))  {
             userSign = "pisces";
             zodiacImg = "./assets/img/zodiac-svg/pisces-astrological-sign-symbol.svg";
         }
 
+        
+        localStorage.setItem("userName", userName);
+        localStorage.setItem("userSign", userSign);
+        localStorage.setItem("zodiacImg", zodiacImg);
+        localStorage.setItem("data", "true")
+        resultPage();
+        location.reload();
+    });
 
 
-
+    console.log(sign)
+    console.log(name)
+    
+    
     // Stop form from submitting normally
     
-
-    // Get some values from elements on the page:
+    function resultPage(){
+        name = localStorage.getItem("userName");
+        sign = localStorage.getItem("userSign");
+        images = localStorage.getItem("zodiacImg");
+        // Get some values from elements on the page:
         birthdate = $("#datepicker").val();
         console.log(birthdate);
-        url = "https://aztro.sameerkumar.website?sign=" + userSign + "&day=today";
-
-
-    // Send the data using post
-    var posting = $.post(url, "");
-
-    // Put the results in a div
-    posting.done(function (data) {
-        //var content = $(data);
-        horoscope = data.description;
-        // $("#result").empty().append("RESPONSE: <pre>" + JSON.stringify(content) + "</pre>");
+        url = "https://aztro.sameerkumar.website?sign=" + sign + "&day=today";
+        
+        
+        
+        // Send the data using post
+        var posting = $.post(url, "");
+        
+        // Put the results in a div
+        posting.done(function (data) {
+            //var content = $(data);
+            horoscope = data.description;
+            // $("#result").empty().append("RESPONSE: <pre>" + JSON.stringify(content) + "</pre>");
         console.log(horoscope);
 
         $.post(
@@ -180,14 +219,14 @@ $(document).on("click", "#send", function (event) {
               'api_key': "472f187a7c554f42d719990a387ff96c",
               'data': horoscope
             })
-          ).then(function(response) { 
-        
+            ).then(function(response) { 
+                
             console.log(response);
 
             $("#horoscope-result").text("");
 
             var zodiacIcon = $("<img>");
-            $(zodiacIcon).attr("src", zodiacImg);
+            $(zodiacIcon).attr("src", images);
             $("#horoscope-result").append(zodiacIcon);
  
             var horoscopeDiv = $("<div>");
@@ -195,9 +234,9 @@ $(document).on("click", "#send", function (event) {
             $("#horoscope-result").append(horoscopeDiv);
 
 
-
+            
            
-
+            
         
             //clear userInput value
             if (response.results >= 0.5) {
@@ -207,32 +246,35 @@ $(document).on("click", "#send", function (event) {
                 predictedDay = "You might have a bad day.";
                 $("#output").text(predictedDay);
             }
-
+            
             console.log(response.results);
             sentiment = response.results;
 
             barWidth = (sentiment.toFixed(2) * 100);
             console.log(barWidth);
-
+            
             $("#pos-bar").css("width", (barWidth + "%"));
-
-          });
-
-          
-        
+            
         });
-
-       
+        
+        
         
     });
+    };
+    
+
+    
+
 
     $("#clear-form").on("click", function () {
+        localStorage.clear();
         location.reload();
     })
+    
 
-// $("#feelingsButton").on("click", function() {
-  
-  // batch example
+    // $("#feelingsButton").on("click", function() {
+        
+        // batch example
  
     
-// });
+        // });
